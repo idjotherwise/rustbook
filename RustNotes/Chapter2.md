3b7038eff044a445d93a4c91b96140f05d77ddf7
Ifanj <ifan.johnston@gmail.com>
2021-11-27 16:39:25 +0100

    some notes on chapter 2: vectors, arrays, matching, looping

--- RustNotes/Chapter2.md
+++ RustNotes/Chapter2.md
@@ -9,3 +9,91 @@ struct Visitor {
 ```
 
 the trailing comma on the last entry is ignored by the compiler, so it is optional.
+
+To implement methods associated with the struct, you do something like
+
+```rust
+impl Visitor {
+	fn new(name: &str, age: &int, greeting: &str) -> Self {
+		Self {
+			name: name.to_lowercase(),
+			age: age,
+			greeting: greeting.to_string(),
+		}
+	}
+	fn greet_visitor(&self) {
+		println!("{}", self.greeting);
+	}
+}
+```
+
+## Arrays and Vectors
+**Arrays** have a _fixed size_, while **Vectors** (`Vec`) are dynamically resizable. Elements can be added with `push()`.
+
+Vectors can be created in a similar way to arrays, with the help of the `vec!` macro: 
+
+```rust
+# array, immutable size:
+let visitor_list: [&str, 2] = [
+"visitor0",
+"visitor1",
+];
+
+# vec! macro, mutable size
+let mut visitor_list = vec![
+"visitor0",
+"visitor1",
+];
+
+# or without the vec! macro:
+let mut visitor_list = Vec::new();
+visitor_list.push(
+	"visitor0"
+);
+...
+
+
+```
+
+Vectors are generally declared as `Vec{T}` for some type `T`, same as in `julia`.
+
+## Debug printing
+To print an instance of a (custom) struct, you need to add the `#[derive(Debug)]` to the struct definition like so:
+```rust
+#[derive(Debug)]
+struct Visitor {
+	name: String,
+	greeting: String,
+}
+```
+then you can print all the information about the instance with:
+
+```rust
+println!("{:?}", Visitor::new("ifan", "hello there"))
+```
+or `"{:#?}"` for pretty printing.
+
+
+## Loops and breaks
+Syntax for running a loop until you break it is:
+```rust
+loop {
+printl!("This is a loop!")
+... // loop is evaluated repeatedly until break is called
+break;
+}
+// execution resumes here after break
+```
+
+
+## Iterators and options
+`array.iter()` will create an iterator from the items of `array` and then calling `.find(|item| condition)` will return `item`s that satisfy `condition` . If no items satsify `condition` then it will return `None`. To deal with this you can use match to check for `something` or `nothing`:
+
+```rust
+match visitor_list {
+	Some(visitor) => visitor.greet_visitor(),
+	None => {
+		// deal with missing match
+	}
+}
+```
--- treehouse/src/main.rs
+++ treehouse/src/main.rs
@@ -2,23 +2,23 @@
 
 use std::io::stdin;
 
+#[derive(Debug)]
 struct Visitor {
-  name: String,
-  greeting: String,
+    name: String,
+    greeting: String,
 }
 
 impl Visitor {
-  fn new(name: &str, greeting: &str) -> Self {
-    Self {
-      name: name.to_lowercase(),
-      greeting: greeting.to_string(),
+    fn new(name: &str, greeting: &str) -> Self {
+        Self {
+            name: name.to_lowercase(),
+            greeting: greeting.to_string(),
+        }
     }
-  }
-  fn greet_visitor(&self) {
-    println!("{}", self.greeting)
-  }
+    fn greet_visitor(&self) {
+        println!("{}", self.greeting);
+    }
 }
-
 
 fn what_is_your_name() -> String {
     let mut your_name = String::new();
@@ -29,21 +29,33 @@ fn what_is_your_name() -> String {
 }
 
 fn main() {
-    let visitor_list: [Visitor; 3] = [
-      Visitor::new("ifan", "Hello ifan, der mewn."),
-      Visitor::new("gruff", "Hi pony, the trombone is in the corner"),
-      Visitor::new("lowri", "Hi lowri, the pumpkin pie is on the tabble")
+    let mut visitor_list = vec![
+        Visitor::new("ifan", "Hello ifan, der mewn."),
+        Visitor::new("dafydd", "Hi pony, the trombone is in the corner"),
+        Visitor::new("johnston", "Hi Johnny, the pumpkin pie is on the tabble"),
     ];
 
+    loop {
+        println!("Hello, what's your name? Leave empty and press ENTER to quit.");
+        let name = what_is_your_name();
+        let known_visitor = visitor_list.iter().find(|visitor| visitor.name == name);
 
-    println!("Hello, what's your name?");
-    let name = what_is_your_name();
-    let known_visitor = visitor_list
-    .iter()
-    .find(|visitor| visitor.name == name);
-    
-    match known_visitor {
-      Some(visitor) => visitor.greet_visitor(),
-      None => println!("You are no on the list, get out!")
+        match known_visitor {
+            // visitor is already on list
+            Some(visitor) => visitor.greet_visitor(),
+            // visitor is not on list, or has no name
+            None => {
+                if name.is_empty() {
+                    // visitor has no name
+                    break;
+                } else {
+                    // visitor has a name but is not on the list
+                    println!("{} is not on the visitor list.", name);
+                    visitor_list.push(Visitor::new(&name, "New friend."));
+                }
+            }
+        }
     }
+    println!("Final list of visitors:");
+    println!("{:#?}", visitor_list);
 }
